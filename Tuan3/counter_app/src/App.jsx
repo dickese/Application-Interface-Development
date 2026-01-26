601e62abf466091ef87b01f9ad65cf7b728101db
Duc Nguyen <nguyenvananhduc2005@gmail.com>
2026-01-26 16:00:14 +0700

    Hoàn thành React Fundamentals & JSX tuần 3 bài 2 counter app

---
+++ Tuan3/counter_app/src/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className=''>
+        <h1>Counter App</h1>
+        <h1>{count}</h1>
+        <div style={{display: "flex", gap: "10px", justifyContent: "center"}}>
+          <button
+            style={{
+              backgroundColor: 'blue',
+              color: 'white',
+              padding: '8px 24px',
+            }}
+            onClick={() => {setCount(prev => {
+
+              const value = prev + 1
+              console.log(`Chọn + : ${prev} + 1 = ${value}`)
+              return value
+            })
+        
+            }}
+          >
+            +
+          </button>
+          <button
+            style={{
+              backgroundColor: 'red',
+              color: 'white',
+              padding: '8px 24px',
+            }}
+            onClick={() => {
+              if (count > 0) {
+                {
+                  setCount((prev) => {
+                    const value = prev - 1;
+                    console.log(`Chọn - : ${prev} - 1 = ${value}`);
+                    return value;
+                  });
+                }
+              }
+            }}
+          >
+            -
+          </button>
+          <button
+            style={{
+              backgroundColor: 'green',
+              color: 'white',
+              padding: '8px 24px',
+            }}
+            onClick={() => {
+              setCount(0)
+              console.log("Chọn reset")
+              return 0
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
